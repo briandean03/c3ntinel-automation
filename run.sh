fca4f5c4f8504f781abd328bbe90a78fb9940628
Brian <justbrian226@gmail.com>
2025-07-16 13:20:03 +0400

    Fix port

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 echo "🔁 Starting FastAPI server..."
-uvicorn main:app --host 0.0.0.0 --port 8000
+
+# Read the Render-assigned port (default to 8000 if not set)
+PORT=${PORT:-8000}
+
+# Run FastAPI server using uvicorn
+uvicorn main:app --host 0.0.0.0 --port $PORT
