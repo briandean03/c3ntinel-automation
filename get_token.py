7a2f59949c93d70681d1220edf29c17f19be1cae
Brian <justbrian226@gmail.com>
2025-07-21 13:42:07 +0400

    made alot of changes to code

---
+++ get_token.py
@@ -0,0 +1,15 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+import os
+
+SCOPES = ['https://www.googleapis.com/auth/drive.file']
+creds = None
+if os.path.exists('token.json'):
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+if not creds or not creds.valid:
+    flow = InstalledAppFlow.from_client_secrets_file('client_secrets.json', SCOPES)
+    creds = flow.run_local_server(port=0)
+    with open('token.json', 'w') as token:
+        token.write(creds.to_json())
+print(f"Refresh Token: {creds.refresh_token}")
